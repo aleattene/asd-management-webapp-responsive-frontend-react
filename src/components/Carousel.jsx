2a8c023a4b93b65826c5a67acfd3880ebeeab63a
aleattene <attene.tomorrowdevs@gmail.com>
2023-03-15 15:29:03 +0100

    feat(home): add homepage v1.0

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
-const Carousel = () => {
+function CarouselThreeSlides() {
     return (
         <div>
-            Carousel
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="bolt_1920_910.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="rossi_1920_910.jpg"
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="federer_1920_910.jpg"
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
         </div>
     );
-};
+}
 
-export default Carousel;
+export default CarouselThreeSlides;
