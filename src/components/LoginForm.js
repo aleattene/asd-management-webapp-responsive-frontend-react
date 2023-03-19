3e5cb780c04c33d7ae27350009f784dcf315723a
aleattene <attene.tomorrowdevs@gmail.com>
2023-03-19 22:39:36 +0100

    feat(login-form): add registration button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,7 +5,7 @@ function LoginForm() {
         <div className="bg-dark mt-1 mb-1">
             <Row>
                 <Col md={{ span: 8, offset: 2 }}>
-                    <Form className="px-3">
+                    <Form className="px-3 mx-auto" style={{minWidth: "500px", maxWidth:"70%"}}>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email"/>
@@ -18,8 +18,13 @@ function LoginForm() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password"/>
                         </Form.Group>
-                        <Button variant="primary" type="submit" className="mt-5 mb-5">
-                            Submit
+                        <Button variant="primary" type="submit" className="mt-5 mb-5 mx-3"
+                                style={{minWidth: "200px", maxWidth: "20%"}}>
+                            Accedi
+                        </Button>
+                        <Button variant="primary" type="submit" className="mt-5 mb-5 mx-3"
+                                style={{minWidth: "200px", maxWidth: "20%", whiteSpace: "nowrap"}}>
+                            Registrazione Utente
                         </Button>
                     </Form>
                 </Col>
