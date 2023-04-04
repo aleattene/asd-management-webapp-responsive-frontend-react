c502df07b148946aa53988457c40a50fefb50df9
aleattene <attene.tomorrowdevs@gmail.com>
2023-04-04 12:53:42 +0200

    feat(documentations): add purchases API /staff/documentations/purchases

--- src/components/pages/staff/documentations/Purchases.jsx
+++ src/components/pages/staff/documentations/Purchases.jsx
@@ -1,36 +1,38 @@
 import React, {useEffect, useState} from 'react';
 import Container from "react-bootstrap/Container";
-import {getProductsService} from "../../../../services/products/products.service";
 import {Table} from "react-bootstrap";
-import StaffNavbar from "../../../StaffNavbar";
-// TO FIX -> PURCHASES
-const Products = () => {
+import {getPurchasesService} from "../../../../services/documentations/purchases.service";
 
-    const [products, setProducts] = useState([])
+const Purchases = () => {
+
+    const [purchases, setPurchases] = useState([])
 
     useEffect(() => {
-        getProducts();
+        getPurchases();
     }, []);
 
-    const getProducts = async () => {
-        const data = await getProductsService();
-        if (data) { setProducts(data) }
+    const getPurchases = async () => {
+        const data = await getPurchasesService();
+        if (data) { setPurchases(data) }
     }
 
     return (
         <div style={{display: 'flex', flexDirection: 'column'}}>
-            <StaffNavbar />
-            <Container fluid>
-                <Table responsive>
-                    <HeaderProducts />
-                {/*JSON.stringify(products)*/}
-                { products.map((prod) => {
+            <Container fluid style={{ padding: "0"}}>
+                <Table responsive hover
+                       style={{ margin: "0",
+                           border: "3px solid #343A40"}}>
+                    <HeaderPurchases />
+                    { /* TO FIX -> remunerations empty or not object */ }
+                    {/*JSON.stringify(products)*/}
+                { purchases.map((purchase) => {
                     return (
-                        <RowProducts
-                            idServizio = { prod.idServizio }
-                            descrizioneServizio = { prod.descrizioneServizio }
-                            costo = { prod.costo }
-                            stato = { prod.stato }
+                        <RowPurchases
+                            date = { purchase.date }
+                            amount = { purchase.amount }
+                            service = { purchase.service }
+                            user = { purchase.user }
+                            paymentMethod = { purchase.paymentMethod }
                         />
                     )
                 })}
@@ -40,18 +42,21 @@ const Products = () => {
     );
 }
 
-const HeaderProducts = () => {
+const HeaderPurchases = () => {
 
     const headers = [
-        "TO FIX",
-        "TO FIX",
-        "TO FIX",
-        "TO FIX"
+        "Data Acquisto",
+        "Importo",
+        "Servizio Acquistato",
+        "Utente",
+        "Metodo di Pagamento"
     ]
 
     return (
-        <thead>
-            <tr style={{bgColor: "red"}}>
+        <thead style={{backgroundColor: "#FFB691"}}>
+        <tr style={{
+            fontWeight: "600",
+            color: "#343A40"}}>
                 {headers.map(h => {
                     return (
                         <td> { h } </td>
@@ -63,14 +68,15 @@ const HeaderProducts = () => {
 };
 
 
-const RowProducts = (props) => {
+const RowPurchases = (props) => {
     return (
-        <tbody>
-            <tr>
-                <td> { props.idServizio } </td>
-                <td> { props.descrizioneServizio }</td>
-                <td> € { props.costo.toFixed(2) } </td>
-                <td> { props.stato } </td>
+        <tbody style={{backgroundColor: "#FFFFF0"}}>
+            <tr style={{borderBottom: "lightgray"}}>
+                <td> { props.date } </td>
+                <td> € { props.amount.toFixed(2) } </td>
+                <td> { props.service } </td>
+                <td> { props.user } </td>
+                <td> { props.paymentMethod } </td>
                 {/* <td>
                     <Form.Check/>
                     <Form.Check disabled/>
@@ -82,4 +88,4 @@ const RowProducts = (props) => {
     )
 }
 
-export default Products;
+export default Purchases;
